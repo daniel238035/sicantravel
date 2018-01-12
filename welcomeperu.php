23de696b0b7e5ed0df196127c6fe74ccd512464b
DANIELCOREI7\Daniel <daniel238035@hotmail.com>
2018-01-12 12:10:28 -0500

    ultima actualizacion 12 01

--- welcomeperu.php
+++ welcomeperu.php
@@ -72,7 +72,7 @@
                             </h2>
                         </div>
                         <div class="col s12 flex-row ">
-                            <h5 class="white-text">TOUR - HOTELES - RESTAURANTES</h5>
+                            <h5 class="white-text"><i class="material-icons">map</i> TOUR - <i class="material-icons">local_hotel</i> HOTELES - <i class="material-icons">local_dining</i> RESTAURANTES</h5>
                         </div>
                         <div class="col s12 flex-row " style="padding-top:50px;">
                             <h5 class="white-text">Conquista la cima de Machu Picchu</h5>
@@ -226,53 +226,48 @@
                     </div>
                     <div class="col s12 l4 ">
                         <div class="row white ">
-                            <div class="col s12 padding-0">
-                                <img class="responsive-img" src="images/portadas/portada10.jpg" alt="" style="height:220px; width:100%;">
+                            <div class="col s12 padding-0 flex-row">
+                                <i class="material-icons color-terciario" style="font-size:100px; margin:40px;">map</i>
                             </div>
                             <div class="col s12 center-align">
-                                <h5 class="text-negrita color-primario">Lujo</h5>
+                                <h5 class="text-negrita color-primario">Te guiamos</h5>
                             </div>
                             <div class="col s12">
-                                <p class="text-justificado color-oscuro">Itinerarios selectos donde todos los detalles han sido cuidados totalmente, explorar Perú por la
-                                    manera más ...</p>
+                                <p class="text-justificado color-oscuro">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vero inventore eum harum debitis in necessitatibus expedita nobis illum dolorum, sit amet repudiandae atque! Quis rem deleniti accusantium blanditiis enim.</p>
                             </div>
-                            <div class="col s12 flex-row padding-10">
-                                <div class="btn btn-terciario" style="width:80%"> Leer Más</div>
-                            </div>
+
                         </div>
                     </div>
         
                     <div class="col s12 l4 ">
                         <div class="row white ">
-                            <div class="col s12 padding-0">
-                                <img class="responsive-img" src="images/portadas/portada8.jpg" alt="" style="height:220px; width:100%;">
+                            <div class="col s12 padding-0 flex-row">
+                                <i class="material-icons color-terciario" style="font-size:100px; margin:40px;">rowing</i>
                             </div>
                             <div class="col s12 center-align">
-                                <h5 class="text-negrita color-primario">Aventura</h5>
+                                <h5 class="text-negrita color-primario">Disfrutaras tu viaje</h5>
                             </div>
                             <div class="col s12">
-                                <p class="text-justificado color-oscuro">Selección de actividades y deportes de aventura que se pueden realizar dentro del territorio peruano...</p>
+                                <p class="text-justificado color-oscuro">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vero inventore eum harum debitis in necessitatibus
+                                    expedita nobis illum dolorum, sit amet repudiandae atque! Quis rem deleniti accusantium blanditiis enim.</p>
                             </div>
-                            <div class="col s12 flex-row padding-10">
-                                <div class="btn btn-terciario" style="width:80%"> Leer Más</div>
-                            </div>
+                        
                         </div>
                     </div>
         
                     <div class="col s12 l4 ">
                         <div class="row white ">
-                            <div class="col s12 padding-0">
-                                <img class="responsive-img" src="images/portadas/portada13.jpg" alt="" style="height:220px; width:100%;">
+                            <div class="col s12 padding-0 flex-row">
+                                <i class="material-icons color-terciario" style="font-size:100px; margin:40px;">hearing</i>
                             </div>
                             <div class="col s12 center-align">
-                                <h5 class="text-negrita color-primario">Agroturismo</h5>
+                                <h5 class="text-negrita color-primario">Estamos en Contácto</h5>
                             </div>
                             <div class="col s12">
-                                <p class="text-justificado color-oscuro">Únete a actividades con guías especializados en áreas rurales, pequeñas ciudades y pueblos para ...</p>
+                                <p class="text-justificado color-oscuro">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vero inventore eum harum debitis in necessitatibus
+                                    expedita nobis illum dolorum, sit amet repudiandae atque! Quis rem deleniti accusantium blanditiis enim.</p>
                             </div>
-                            <div class="col s12 flex-row padding-10">
-                                <div class="btn btn-terciario" style="width:80%"> Leer Más</div>
-                            </div>
+                        
                         </div>
                     </div>
         
@@ -282,6 +277,35 @@
 
     </div>
 
+
+    <footer class="row page-footer bg-negro ">
+        <div class="container">
+            <div class="row">
+                <div class="col l4 s12">
+                    <h5 class="white-text">CONTACTATE CON NOSOTROS</h5>
+                    <p class="color-oscuro titulo-item ">Direccíón: Av.Balta 123</p>
+                    <p class="color-oscuro titulo-item ">Teléfono: 074-235689</p>
+                    <p class="color-oscuro titulo-item ">Correo: agencia@gmail.com</p>
+                </div>
+                <div class="col l4 s12 center hide-on-med-and-down ">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.5075712598714!2d-79.90641178552958!3d-6.707735367445322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x904ced9307db00dd%3A0xd82bb63ce69b1c52!2sUniversidad+Nacional+Pedro+Ru%C3%ADz+Gallo!5e0!3m2!1ses!2spe!4v1513137581810"
+                        width="100%" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+                <div class="col l4 s12">
+                    <h5 class="white-text">HOJA INFORMATIVA</h5>
+                    <div class="row lista-redes-footer">
+                        <p class="color-oscuro  titulo-item">Ingrese su correo electrónico y le enviaremos más información</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright sub-footer">
+            <div class="container">
+                © 2017 Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#">Inicio</a>
+            </div>
+        </div>
+    </footer>
 
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
